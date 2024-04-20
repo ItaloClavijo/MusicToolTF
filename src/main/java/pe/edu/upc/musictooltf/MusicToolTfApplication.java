06e74257cd99c1e38df953fd15cc6b6830d3e553
ItaloClavijo <u201711622@upc.edu.pe>
2024-04-20 11:27:37 -0500

    Initial commit

---
+++ src/main/java/pe/edu/upc/musictooltf/MusicToolTfApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.musictooltf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicToolTfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicToolTfApplication.class, args);
+	}
+
+}
